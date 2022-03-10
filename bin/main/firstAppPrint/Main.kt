5641259759bb8d1ee45c91979674ca3946cffa42
kotamadeo <kotamadeo@gmail.com>
2022-03-10 20:55:07 +0300

    Project's files added.

---
+++ bin/main/firstAppPrint/Main.kt
@@ -0,0 +1,7 @@
+package firstAppPrint
+
+fun main(args: Array<String>) {
+    val ANSI_RESET = "\u001B[0m"
+    val ANSI_PURPLE = "\u001B[35m"
+        println("$ANSI_PURPLE My first app!$ANSI_RESET")
+    }
